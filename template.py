531528e5574fbb131d9fe438c45418287f8f1944
Ahmad Zaki Firdaus <ahmzakif@gmail.com>
2024-10-26 18:08:20 +0700

    edit path

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-project_name = "cnnClassifier"
+project_name = "classifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
