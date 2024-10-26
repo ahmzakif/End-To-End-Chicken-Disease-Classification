268dfb6e97f00a165fe2c90c85e7aa72843d773e
Ahmad Zaki Firdaus <ahmzakif@gmail.com>
2024-10-27 03:10:07 +0700

    base model added

--- src/classifier/config/configuration.py
+++ src/classifier/config/configuration.py
@@ -1,5 +1,5 @@
 from classifier.constants import *
-from classifier.entity.config_entity import DataIngestionConfig
+from classifier.entity.config_entity import *
 from classifier.utils.common import create_directories, read_yaml
 
 
@@ -15,16 +15,34 @@ class ConfigurationManager:
         create_directories([self.config.artifacts_root])
 
 
-def get_data_ingestion_config(self) -> DataIngestionConfig:
-        config = self.config.data_ingestion
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+            config = self.config.data_ingestion
 
+            create_directories([config.root_dir])
+
+            data_ingestion_config = DataIngestionConfig(
+                root_dir=config.root_dir,
+                source_URL=config.source_URL,
+                local_data_file=config.local_data_file,
+                unzip_dir=config.unzip_dir 
+            )
+
+            return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
         create_directories([config.root_dir])
 
-        data_ingestion_config = DataIngestionConfig(
-            root_dir=config.root_dir,
-            source_URL=config.source_URL,
-            local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir 
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
         )
 
-        return data_ingestion_config
+        return prepare_base_model_config
